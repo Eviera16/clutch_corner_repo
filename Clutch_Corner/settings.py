d5c15665d0a470bfd13d45c45c0780e8a085482d
Eviera16 <64331763+Eviera16@users.noreply.github.com>
2022-11-13 15:07:26 -0500

    adding cors allow all true

--- Clutch_Corner/settings.py
+++ Clutch_Corner/settings.py
@@ -98,10 +98,7 @@ DATABASES = {
     }
 }
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-  'http://localhost:3000',
-)
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
